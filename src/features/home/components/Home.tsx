00148ee56ebe85387fe688d288240235783a41c7
Marcello Crescimbeni <107230301+MarcelloCQ@users.noreply.github.com>
2025-03-19 20:50:33 -0500

    Revert "feature resize-img agregada"

--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -1,17 +1,11 @@
 import texts from './../../../texts.json';
-import logoRamen from '/ramen.svg';
 
 const Home = () => {
   return (
     <section className='section'>
       <div className='section--xl'>
         <h1 className='text-center mb-1'>{texts['/home'].title}</h1>
-        <p className='text-center mb-2'>{texts['/home'].description}</p>
-        <div className='container-logo'>
-          <img src={logoRamen} className='logo-ramen-x1' alt='' />
-          <img src={logoRamen} className='logo-ramen-x3' alt='' />
-          <img src={logoRamen} className='logo-ramen-x6' alt='' />
-        </div>
+        <p className='text-center mb-1'>{texts['/home'].description}</p>
       </div>
     </section>
   );
