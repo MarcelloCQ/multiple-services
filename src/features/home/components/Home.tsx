4554fd8aad0d340a6d0f05d4b553036253d3b40e
Marcello <giosuemc@gmail.com>
2025-03-21 11:45:52 -0500

    se retira correo para evitar scripting de datos y mensajes spam

--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -36,14 +36,6 @@ const Home = () => {
             >
               {texts["/home"].button2}
             </a>
-            {/* <Button
-              classname={"button-primary"}
-              textbutton={texts["/home"].button1}
-            />
-            <Button
-              classname={"button-outline"}
-              textbutton={texts["/home"].button2}
-            /> */}
           </div>
         </div>
 
@@ -252,25 +244,6 @@ const Home = () => {
           </p>
 
           <div className="social-links">
-            <a
-              href={texts["/home"]["contact-me"].anchors.mail}
-              className="social-icon"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <rect width="20" height="16" x="2" y="4" rx="2" />
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-              </svg>
-            </a>
             <a
               href={texts["/home"]["contact-me"].anchors.github}
               target="_blank"
