3869bf4fb9a82c2acb22035b7dcfd07c780a7354
Marcello Crescimbeni <giosuemc@gmail.com>
2025-03-23 02:22:27 -0500

    se implemento lazy para cargar rutas

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,13 @@
+import './loader.css';
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <div className='bar bar1'></div>
+      <div className='bar bar2'></div>
+      <div className='bar bar3'></div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,26 +1,47 @@
-import { createBrowserRouter } from "react-router";
-import HomeFeature from "../features/home";
-import ExcelConverterFeature from "../features/excel-converter";
-import ResizeImgFeature from "../features/resize-img";
-import routes from "./routes.json";
-import ContactFormFeature from "../features/contact-form";
+import { createBrowserRouter } from 'react-router';
+import React, { Suspense } from 'react';
+import routes from './routes.json';
+
+const HomeFeature = React.lazy(() => import('../features/home'));
+const ExcelConverterFeature = React.lazy(
+  () => import('../features/excel-converter')
+);
+const ResizeImgFeature = React.lazy(() => import('../features/resize-img'));
+const ContactFormFeature = React.lazy(() => import('../features/contact-form'));
+const Loader = React.lazy(() => import('../components/loader/Loader'));
 
 const router = createBrowserRouter([
   {
     path: routes[0].path,
-    element: <HomeFeature />,
+    element: (
+      <Suspense fallback={<Loader />}>
+        <HomeFeature />
+      </Suspense>
+    ),
   },
   {
     path: routes[1].path,
-    element: <ExcelConverterFeature />,
+    element: (
+      <Suspense fallback={<Loader />}>
+        <ExcelConverterFeature />
+      </Suspense>
+    ),
   },
   {
     path: routes[2].path,
-    element: <ResizeImgFeature />,
+    element: (
+      <Suspense fallback={<Loader />}>
+        <ResizeImgFeature />
+      </Suspense>
+    ),
   },
   {
     path: routes[3].path,
-    element: <ContactFormFeature />,
+    element: (
+      <Suspense fallback={<Loader />}>
+        <ContactFormFeature />
+      </Suspense>
+    ),
   },
 ]);
 
